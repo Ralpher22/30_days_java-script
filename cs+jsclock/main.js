86826d9c1b64ee7140816c9a5539b454aabf31c8
Ralpher22 <rickyralph116@gmail.com>
2024-05-19 02:16:41 +0100

    updated clock

---
+++ cs+jsclock/main.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360; 
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6; 
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30; 
+
+ 
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+
+setDate();
+setInterval(setDate, 1000); 
